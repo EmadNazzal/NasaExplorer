0d7587d99db53f8613843a5924ffff9b54d2c84d
EmadNazzal <emadnazzal95@gmail.com>
2025-06-25 12:40:41 +0100

    testing TimeOut

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+
 const nasaRoutes = require("./routes/nasaRoutes");
 const groqRoutes = require("./routes/groqRoutes");
 
 const app = express();
+app.options("*", cors());
 
 // Allowed frontend origins
 const allowedOrigins = [
@@ -33,7 +35,7 @@ app.use(
 );
 
 // Handle OPTIONS preflight requests for all routes
-app.options("*", cors());
+// app.options("*", cors());
 
 // Parse JSON bodies
 app.use(express.json());
